12c192704e835943445c558f2711949023814c32
Ek29 <ek29seiba@gmail.com>
2020-09-15 12:58:35 +0200

    Update dbTest.py

--- dbManager/dbTest.py
+++ dbManager/dbTest.py
@@ -8,26 +8,21 @@ name = "Immacolata"
 ticketNum = 100
 cost = 10
 
-eventID = 0 #dbManager.add(date, startTime, endTime, name, ticketNum, cost)
+eventID = dbManager.add(date, startTime, endTime, name, ticketNum, cost)
 
 password = []
 for i in range(0,ticketNum):
     password.append(str(i)) 
-print(dbManager.passwordFill(eventID, password))
-
-
+dbManager.passwordFill(eventID, password)
 
 eventInfo = {}
 eventInfo["EN"] = "Hello there!"
-eventInfo["IT"] = "ciao pippini mi sento molto felice                                                                                                                                                                                                                       ok qui dovrei avere il breakpoint"
+eventInfo["IT"] = "Ciao!"
 eventInfo["PL"] = "I don't know Polish!"
 eventInfo["URLs"] = "https:what?"
 dbManager.infoFill(eventID, eventInfo)
-print(dbManager.dailySchedule(date))
-
+dbManager.dailySchedule(date)
 passwrd = dbManager.ticketRetrieve(eventID,"ek29seiba@gmail.com")
-print(passwrd)
-
-print(dbManager.gateAccess(eventID,passwrd))
+dbManager.gateAccess(eventID,passwrd)
 input()
 dbManager.delete(eventID)
