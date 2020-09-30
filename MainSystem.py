b2d1d0baa27f33619697680b9a939cdacf7034fe
Lorenzo <48414987+LorenzoGiaiPron@users.noreply.github.com>
2020-09-30 18:05:01 +0200

    working correctly with the interface
    request for daily events and request for newTicket processed correctly by main system and interface

--- MainSystem.py
+++ MainSystem.py
@@ -12,10 +12,10 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from pswUpdateMQTTClient import PswMQTTClient
-from DeviceCatalogManager import catalog
 import dbManager.dbManager as dbManager
+import os
 import png
-
+from systemConfigurationManager import SysConfig
 testEV = 1  # enable fake events  for testing
 
 # MAIN CONTROL OF THE BOOKING SYSTEM, ACCESSIBLE THROUGH REST APIs
@@ -23,7 +23,6 @@ testEV = 1  # enable fake events  for testing
 # password retrieval from database, conversion to QR code and delivery of code to user through email
 # MQTT communication with multiple gates to send acceptable passwords
 
-# TO-DO: update to the new DB APIs
 DEBUG = True
 dailyEventsPswTable = None
 catalogData = None
@@ -31,6 +30,8 @@ remoteDBparams = {"dbname": "dbuwxucc", "user": "dbuwxucc", "password":"VNx-4S_l
                    "host": "kandula.db.elephantsql.com", "port": "5432"}
 configFileName = "configFile.json"
 DBconn = None  # connection object to access the database, instantiated by the start PUT command to the system
+
+ERROR_IMG_NAME = "error_img.png"
 
 
 class VirtualTicketHandler:
@@ -75,7 +76,7 @@ class VirtualTicketHandler:
             pdf.output(self.pdfPath)
 
     # NOTE: to speed things up, a connection can be established only once instead of each time
-    def sendMail(self, name, receiver_email, event, n_tickets):
+    def sendMail(self, receiver_email, event, n_tickets):
         if DEBUG:
             print("sending e-mail")
         mailAddress = "BookingSystemTest2@gmail.com"
@@ -90,12 +91,12 @@ class VirtualTicketHandler:
             sender_email = mailAddress
             if n_tickets == 1:
                 subject = f"Virtual ticket for event: {event}"
-                body = f"""Dear {name},\n
+                body = f"""
                 Your Virtual Ticket for the event: -{event}- is in the pdf attached to this e-mail
                 """
             else:
                 subject = f"Virtual tickets for event: {event}"
-                body = f"""Dear {name},\n
+                body = f"""
                 Your Virtual Tickets for the event: -{event}- are in the pdf attached to this e-mail
                 """
             # Create a multipart message and set headers
@@ -135,7 +136,7 @@ class VirtualTicketHandler:
                 server.sendmail(sender_email, receiver_email, text)
 
     # wrapper function
-    def sendTicket(self, name, mail_addr, event, passwordList, eventID, scheduledtime):
+    def sendTicket(self, mail_addr, event, passwordList, eventID, scheduledtime):
         if type(passwordList) is list:
             i = 0
             for el in passwordList:
@@ -146,7 +147,7 @@ class VirtualTicketHandler:
             self.pswToQr(password=passwordList, filepath=self.imgPath, eventID=eventID)  # generate a png file in the directory
             self.generatePDF(1, scheduledtime, event)  # generate a pdf containing the QR png in the directory
 
-        self.sendMail(name, mail_addr, event, len(passwordList))
+        self.sendMail(mail_addr, event, len(passwordList))
 
 
 class MainSystemMQTT(PswMQTTClient):
@@ -203,10 +204,11 @@ class JobSchedulerThread(threading.Thread):
         subTopics = catalogData.topics["pswUsed"]
         # start MQTT client
         self.MQTTClient = MainSystemMQTT(clientID="MainSystem", brokerPort=int(catalogData.broker["port"]),
-                                         MQTTbroker=catalogData.broker["ip"], msgTopics=catalogData.topics,
+                                         MQTTbroker=catalogData.broker["ip"], msgTopics=catalogData.getTopics(),
                                          subscribedTopics=subTopics, initialPswTable=dailyEventsPswTable)
         try:
             self.MQTTClient.start()
+            pass
         except:
             raise Exception("Could not start MQTT client, check if the configuration is correct")
         self.on = True
@@ -219,7 +221,7 @@ class JobSchedulerThread(threading.Thread):
         eventDate = datetime.date(2020, 10, 12)  # 12-10-2020, DEBUG DATE
         # eventDate = datetime.date.today()
         print("Retrieving daily table from database...")
-        receivedDailyEvents = dbManager.dailySchedule(eventDate, passFlag=True, conn=DBconn)
+        receivedDailyEvents = dbManager.dailySchedule(eventDate, passFlag=True, conn=DBconn, languages=None)
         print("Daily table retrieved")
         self.dailyTableTMS = datetime.datetime.now().date()  # get date of current day
         # received dictionary:
@@ -287,6 +289,7 @@ class JobSchedulerThread(threading.Thread):
 
 # RESTful interface exposed through cherrypy
 class MainSystemREST(object):
+    ERROR_IMG_NAME = "error_img.png"
     schedulerActive = False  # flag to keep track of scheduler thread
     # generate some TEST events
     global testEV
@@ -317,12 +320,35 @@ class MainSystemREST(object):
     requestScheduler = None  # this class must be initialized through the REST API
     initialized = False
 
+    storedImgs = []  # list of stored image names
+    storedImgsDirChecked = False
+
     def GET(self, *uri, **params):
         global dailyEventsPswTable
         # requests URL formats:
         # used for commands sent by the GUI(user side) or the system management interface that request a resource
         # gates can also call a GET req to retrieve the daily psw table
         if len(uri) != 0:
+            if len(uri) == 1:
+                raise cherrypy.HTTPError(404, "invalid url")
+            if uri[0] == "img":  # image repository
+                print(self.storedImgs)
+                if uri[1] not in self.storedImgs:  # check if requested image is in repository
+                    if not self.storedImgsDirChecked:
+                        # see if directory has already been checked
+                        self.storedImgs.clear()
+                        # get list of stored imgs in directory
+                        self.storedImgs = os.listdir("eventImgs")
+                        self.storedImgsDirChecked = True
+                        if uri[1] not in self.storedImgs:
+                            return cherrypy.HTTPError(404, "image not found")
+                    else:
+                        return cherrypy.HTTPError(404, "image not found")
+
+                cherrypy.response.headers['Content-Type'] = "image/png"  # specify response is image in the header
+                with open('eventImgs/'+uri[1], "rb") as imageFile:
+                    return imageFile.read()  # return byte object
+
             if uri[0] == "customer":
                 if not self.initialized:
                     raise cherrypy.HTTPError(500, "system not initialized, call the PUT request to initialize")
@@ -336,6 +362,12 @@ class MainSystemREST(object):
                         raise cherrypy.HTTPError(500, 'missing parameter "date" in the request')
                     eventDate = self.getDate(params["date"])
                     outputEventsList = self.getEventList(eventDate, language)
+                    # return "[{ 'id': '0', name: 'Fire and Forget at KW Institute for Contemporary Art', patch_img: 'img/events/event1_1.jpg', patch_img2: 'img/events/event1_2.jpg', patch_img3: 'img/events/event1_3.jpg', description: 'Test desc 0', price: '0' }]"
+                    # outputDict = [{"id": '0', "name": "Fire and Forget at KW Institute for Contemporary Art", "patch_img": 'img/events/event1_1.jpg', "patch_img2": 'img/events/event1_2.jpg', "patch_img3": 'img/events/event1_3.jpg', "description": 'Test desc 0', "price": '0'}]
+                    # outputDict = [{"id": '0', "name": "Fire and Forget at KW Institute for Contemporary Art", "patch_img": 'https://i.imgur.com/PvL7PAw.jpg', "patch_img2": 'https://i.imgur.com/AKZI0JO.jpg', "patch_img3": 'https://i.imgur.com/qmrQAKk.jpg', "description": 'Test desc 0', "price": '0'}]
+                    # return json.dumps(outputDict)
+                    if outputEventsList is None:
+                        return ""
                     return json.dumps(outputEventsList)
                 else:
                     raise cherrypy.HTTPError(404, "incorrect URL")
@@ -358,8 +390,11 @@ class MainSystemREST(object):
         global testEV
         global catalogData
         global DBconn
+        global DEBUG
         # sent by GUI or system interface to request an action
         if len(uri) != 0:
+            if len(uri) == 1:
+                raise cherrypy.HTTPError(404, "invalid url")
             # customer services
             if uri[0] == "customer":
                 if uri[1] == "newTicket":  # return the chosen password
@@ -377,20 +412,21 @@ class MainSystemREST(object):
                         Reqdict = json.loads(ReqjsonStr)
                     except:
                         raise cherrypy.HTTPError(500, "could not load JSON string in body")
+                    if DEBUG:
+                        print("received new ticket req with body: ", Reqdict)
                     # check format of body
-                    if not("name" in Reqdict and "eMail" in Reqdict and "event_name" in Reqdict and "eventID" in Reqdict
-                           and "n_tickets" in Reqdict and "start" in Reqdict) and "end" in Reqdict:
+                    if not("eMail" in Reqdict and "language" in Reqdict and "eventID" in Reqdict and "n_tickets" in Reqdict):
                         raise cherrypy.HTTPError(500, "invalid request format")
                     # send eventID, timeslot, e-mail, n_tickets to DB, DB returns n passwords (JSON  list)
 
-                    # ACCESS DB THROUGH APIs
+                    # ACCESS DB THROUGH APIs TO RETRIEVE TICKETS
                     if int(Reqdict["n_tickets"]) > 1:
                         # DB API CALL
                         ticket_remaining = int(dbManager.ticketLeftCheck(Reqdict["eventID"], conn=DBconn))
                         if int(Reqdict["n_tickets"]) > ticket_remaining:
                             return "no ticket available"
                         sel_psw = []
-                        for i in range(0, Reqdict["n_tickets"]):
+                        for i in range(0, int(Reqdict["n_tickets"])):
                             returnVal = dbManager.ticketRetrieve(Reqdict["eventID"], Reqdict["eMail"], conn=DBconn)
                             if returnVal is None:
                                 break  # no more tickets
@@ -407,11 +443,21 @@ class MainSystemREST(object):
                             return "no ticket available"
                     # send password to the user
                     if DEBUG:
-                        print(f'sending QR code to {Reqdict["name"]}, mail:{Reqdict["eMail"]} for event: {Reqdict["event_name"]}')
+                        print(f'sending QR code to mail:{Reqdict["eMail"]}, language {Reqdict["language"]}')
 
                     print("sending psw list:", sel_psw)
-                    self.virtualTicketHandler.sendTicket(Reqdict["name"], Reqdict["eMail"], Reqdict["event_name"],
-                                                         sel_psw, Reqdict["eventID"], (Reqdict["start"], Reqdict["end"]))
+                    # DB API to retrieve name in specified language
+                    rx_info = dbManager.retreiveInfo(str(Reqdict["eventID"]), DBconn)
+                    if Reqdict["language"] == "uk":
+                        lang = "EN"
+                    elif Reqdict["language"] == "pl":
+                        lang = "PL"
+                    elif Reqdict["language"] == "pl":
+                        lang = "IT"
+                    else:
+                        lang = "EN"
+                    self.virtualTicketHandler.sendTicket(Reqdict["eMail"], rx_info[lang]["name"], sel_psw,
+                                                         Reqdict["eventID"], (rx_info["startTime"], rx_info["endTime"]))
 
                     # insert password in daily table and send to gates IF the event is in daily table
                     if str(Reqdict["eventID"]) in dailyEventsPswTable:  # check if event in daily table
@@ -431,9 +477,12 @@ class MainSystemREST(object):
                             json_msg = json.dumps(dict_msg)
                             self.requestScheduler.MQTTClient.publish(catalogData.topics["newPsw"], json_msg)
                     return json.dumps(sel_psw)  # return the passwords
-
+                else:
+                    raise cherrypy.HTTPError(404, "invalid url")
             # system management
             if uri[0] == "system":
+                if len(uri) == 1:
+                    raise cherrypy.HTTPError(404, "invalid url")
                 # management of the MQTT client
                 if uri[1] == "start":  # THIS SHOULD ALWAYS BE CALLED AFTER SCRIPT IS EXECUTED
                     print("received start request")
@@ -489,20 +538,40 @@ class MainSystemREST(object):
                                 self.requestScheduler.changeTime(newTime)
                             else:
                                 raise cherrypy.HTTPError(403, "incorrect parameter format")
+            else:
+                raise cherrypy.HTTPError(404, "invalid url")
 
+    # THIS REST METHOD IS ONLY USED BY THE SYSTEM MANAGER TO UPLOAD IMAGES
+    def POST(self, *uri, **param):
+        if len(uri) != 0:
+            if len(uri) == 1:
+                raise cherrypy.HTTPError(404, "invalid url")
+            if uri[0] == "img":
+                imgName = uri[1]
+                if imgName not in self.storedImgs:
+                    self.storedImgs.append(imgName)
+                raw_payload = cherrypy.request.body.read()
+                # print(type(raw_payload))
+                with open('eventImgs/'+imgName, "wb") as fp:
+                    fp.write(raw_payload)  # write object file as img
             else:
                 raise cherrypy.HTTPError(404, "invalid url")
 
     def getEventList(self, date, language):
         global DBconn
+        global catalogData
         # date is a datetime.date object
         # format {1: {'ticketNum': 100, 'ticketLeft': 100, 'cost': 10, 'startTime': '03:00', 'endTime': '04:00',
         # 'EN': {'name': 'EN_test_name', 'info': 'EN_test_desc'}, 'IT': {'name': 'IT_test_name', 'info': 'IT_test_desc'}
         # , 'PL': {'name': 'PL_test_name', 'info': 'PL_test_desc'}, 'URLs': ['testurl1', 'testurl2']}
-        eventDict = dbManager.dailySchedule(date, passFlag=False, conn=DBconn)
+        if language == "all":
+            reqLanguage = ["EN","IT","PL"]
+        else:
+            reqLanguage = [language]
+        eventDict = dbManager.dailySchedule(date, passFlag=False, conn=DBconn, languages=reqLanguage)
         if not eventDict:
             # dictionary is empty
-            return []
+            return None
         else:
             # order events by start time
             sortedEvents = sorted(list(eventDict.items()), key=lambda x: int(x[1]["startTime"].split(':')[0])) # ascending by start hour
@@ -516,25 +585,48 @@ class MainSystemREST(object):
                                               "ticketLeft": dataDict["ticketLeft"], "cost": dataDict["cost"],
                                               "time": [dataDict["startTime"], dataDict["endTime"]], "URLs": dataDict["URLs"]}}
                 else:
-                    tmpDict = {str(eventID):{language:dataDict[language], "ticketLeft": dataDict["ticketLeft"],
-                                             "cost":dataDict["cost"], "time":[dataDict["startTime"],dataDict["endTime"]],
-                                             "URLs":dataDict["URLs"]}}
+                    languageInfo = dataDict[language]
+                    tmpDict = {"id": str(eventID), "name": languageInfo["name"], "description": languageInfo["info"],
+                               "price": str(dataDict["cost"]), "ticketLeft": int(dataDict["ticketLeft"])}
+                    # TO-DO: add ticketLeft, start and end time once they are implemented in interface
+                    # add img urls
+                    imgURL_prefix = catalogData.urls["mainSystem"]+'/img/'
+                    if dataDict["URLs"][0] is None:
+                        tmpDict["patch_img"] = imgURL_prefix+self.ERROR_IMG_NAME
+                    else:
+                        tmpDict["patch_img"] = imgURL_prefix + dataDict["URLs"][0]
+
+                    if dataDict["URLs"][1] is None:
+                        tmpDict["patch_img2"] = imgURL_prefix+self.ERROR_IMG_NAME
+                    else:
+                        tmpDict["patch_img2"] = imgURL_prefix + dataDict["URLs"][1]
+
+                    if dataDict["URLs"][2] is None:
+                        tmpDict["patch_img3"] = imgURL_prefix+self.ERROR_IMG_NAME
+                    else:
+                        tmpDict["patch_img3"] = imgURL_prefix + dataDict["URLs"][2]
                 outputList.append(tmpDict)
             return outputList
 
     def getDate(self, dateStr):
-        dateList = dateStr.split('-')
+        cutDateStr = dateStr[0:10]
+        print("CUT STR:", cutDateStr)
+        dateList = cutDateStr.split('-')
         try:
-            return datetime.date(int(dateList[2]), int(dateList[1]), int(dateList[0]))
+            return datetime.date(int(dateList[0]), int(dateList[1]), int(dateList[2]))
         except:
             print("incorrect date format, use DD-MM-YYYY")
             raise cherrypy.HTTPError(500, "Error: incorrect date format, use DD-MM-YYYY")
+
 
 if __name__ == '__main__':
     # open configuration file
     with open(configFileName, "r") as fp:
         config = json.load(fp)
-    catalogData = catalog(config["resourceCatalog"]["ip"], config["resourceCatalog"]["port"])
+    if "DBparams" not in config:
+        raise KeyError("access parameters to the remote DB are missing from the configuration file, "
+                       "('DBparams' key is missing)")
+    catalogData = SysConfig(config["DBparams"])
     catalogData.requestAll()
     conf = {
         '/': {
